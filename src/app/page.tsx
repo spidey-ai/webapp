986bdb8dee060acbe6a5205d6cb5fa70d15041b6
Jaivin Wylde <me@jai.vin>
2024-04-29 14:51:17 -0700

    remove some stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,19 +58,3 @@ export default async function Home() {
     </>
   )
 }
-
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest()
-
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
-  )
-}
